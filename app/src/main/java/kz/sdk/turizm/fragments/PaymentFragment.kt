f93eeb4196c7de7076a574e3c9ed2fd44af77457
dinmukhamed2517 <123656634+dinmukhamed2517@users.noreply.github.com>
2024-05-08 19:19:44 +0500

    commit 4

--- app/src/main/java/kz/sdk/turizm/fragments/PaymentFragment.kt
+++ app/src/main/java/kz/sdk/turizm/fragments/PaymentFragment.kt
@@ -22,38 +22,6 @@ class PaymentFragment:BaseFragment<FragmentPaymentBinding>(FragmentPaymentBindin
     private fun setupInputFields() {
         binding.etCardNumber.filters = arrayOf(InputFilter.LengthFilter(16))
         binding.etCvc.filters = arrayOf(InputFilter.LengthFilter(3))
-
-        binding.etDate.addTextChangedListener(object : TextWatcher {
-            private var current = ""
-            private val mmYyPattern = Regex("(\\d{1,2})(\\d{1,2})?")
-
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
-
-            override fun afterTextChanged(s: Editable?) {
-                s?.let {
-                    val input = s.toString()
-                    if (input != current) {
-                        val match = mmYyPattern.find(input.replace("/", ""))?.groupValues
-
-                        if (match != null) {
-                            var month = match[1]
-                            var year = match.getOrNull(2) ?: ""
-
-                            if (month.length == 1 && month.toInt() > 1) {
-                                month = "0$month/"
-                            } else if (month.length == 2) {
-                                if (month.toInt() > 12) month = "12"
-                                month += "/"
-                            }
-
-                            current = "$month$year"
-                            s.replace(0, s.length, current)
-                        }
-                    }
-                }
-            }
-        })
+        binding.etDate.filters = arrayOf(InputFilter.LengthFilter(5))
     }
 }
